86dff45995892ca21f384199e498f00247ddc96a
hackerized1 <50502173+hackerized1@users.noreply.github.com>
2022-12-14 14:45:08 +0300

    Update javaphpmysql-Sorgupanel.php

--- javaphpmysql-Sorgupanel.php
+++ javaphpmysql-Sorgupanel.php
@@ -6,20 +6,20 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-if(isset($_POST['tcno'],$_POST['ad'],$_POST['soyad'],$_POST['tel'],$_POST['babaadi'],$_POST['anneadi'])){
-    $tcno=$_POST['tcno'];
+if(isset($_POST['no'],$_POST['ad'],$_POST['soyad'],$_POST['tel'],$_POST['babaadi'],$_POST['anneadi'])){
+    $tcno=$_POST['no'];
     $ad=$_POST['ad'];
     $soyad=$_POST['soyad'];
     $tel=$_POST['tel'];
     $babaadi=$_POST['babaadi'];
     $anneadi=$_POST['anneadi'];
     
-    $sql = "SELECT * FROM bilgiler WHERE tcno='$tcno' AND ad='$ad' AND soyad='$soyad' AND tel='$tel' AND babaadi='$babaadi' AND anneadi='$anneadi'";
+    $sql = "SELECT * FROM bilgiler WHERE no='$tcno' AND ad='$ad' AND soyad='$soyad' AND tel='$tel' AND babaadi='$babaadi' AND anneadi='$anneadi'";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($result)) {
-            echo "TCNO: " . $row["tcno"]. " - Name: " . $row["ad"]. " " . $row["soyad"]. "<br>";
+            echo "NO: " . $row["no"]. " - Name: " . $row["ad"]. " " . $row["soyad"]. "<br>";
         }
     } else {
         echo "0 results";
@@ -31,7 +31,7 @@ if(isset($_POST['tcno'],$_POST['ad'],$_POST['soyad'],$_POST['tel'],$_POST['babaa
 
 // HTML Kodu
 <form action="" method="post">
-    TCNO: <input type="text" name="tcno" required><br>
+    TCNO: <input type="text" name="no" required><br>
     Ad: <input type="text" name="ad" required><br>
     Soyad: <input type="text" name="soyad" required><br>
     Tel: <input type="text" name="tel" required><br>
